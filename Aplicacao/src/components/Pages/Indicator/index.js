2cbe144cb45144c08fca6d756d115a02ca8de225
João Gabriel <jgmp11@gmail.com>
2021-02-01 13:49:26 -0300

    Adicionando a pagina de informações

--- Aplicacao/src/components/Pages/Indicator/index.js
+++ Aplicacao/src/components/Pages/Indicator/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import {Container} from 'react-bootstrap';
+import ListGroup from 'react-bootstrap/ListGroup'
 
 export default function Indicator(){
     return(
-        <div>Indicator</div>
+        <ListGroup>
+            <h1>Informações</h1>
+            <ListGroup.Item>Dispositivo: </ListGroup.Item>
+            <ListGroup.Item>TS: </ListGroup.Item>
+            <ListGroup.Item>Latitude: </ListGroup.Item>
+            <ListGroup.Item>Longitude</ListGroup.Item>
+            <ListGroup.Item>Bateria: </ListGroup.Item>
+        </ListGroup>
     )
 }
