a120b0fb3183e24ee8f9bfeb52d77031926723fa
THETNT\User <renatotbreis@gmail.com>
2021-02-03 10:48:21 -0300

    /csv
    -> gps.csv adicionado ao repositório;
    -> follow-up nos updates da rota /csv

--- API/modules/csv_route.py
+++ API/modules/csv_route.py
@@ -9,6 +9,7 @@ warnings.filterwarnings ('ignore')
 
 @csv_route.get ('/csv/{collection}')
 def csv (collection: str):
+  col = collection
   collection = MongodbConnector ().get_collection (collection)
   mongo_docs = collection.find ()
   mongo_docs = list (mongo_docs)
@@ -21,9 +22,12 @@ def csv (collection: str):
     series_obj = pandas.Series (doc, name = doc_id)
     docs = docs.append (series_obj)
 
+  return docs.to_csv (sep = ',')
+  
+'''
   try:
-    docs.to_csv (path_or_buf = f'./{collection}.csv', sep = ',')
+    docs.to_csv (path_or_buf = f'./{col}.csv', sep = ',')
     return 200
   except Exception as err:
     return err
-  
+'''
