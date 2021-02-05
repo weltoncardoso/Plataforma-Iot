dd2e332180e5a8fa0fed831a8378a7a9de0e608a
Arley Souto <macx15@hotmail.com>
2021-02-05 13:28:57 -0300

    Criado pagina de cadastro

---
+++ Aplicacao/src/components/Pages/Cadastro/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Container} from 'react-bootstrap';
+
+export default function Cadastro(){
+    return(
+        <Container>
+            <h1>Cadastro</h1>
+        </Container>
+    )
+}
